d2c4b9db2381973eddcf267842954ce71c6a3dd0
Steven Londoño <stlondonoh@udistrital.edu.co>
2025-09-26 20:32:39 -0500

    Completada ventana inicio con verificación.

--- Documents/PRIMER-TALLER-PA/RolaPET/src/control/ControlVentanaInicio.java
+++ Documents/PRIMER-TALLER-PA/RolaPET/src/control/ControlVentanaInicio.java
@@ -3,22 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package control;
+
 import vista.VentanaInicio;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-/**
- *
- * @author Steven
- */
-
 public class ControlVentanaInicio implements ActionListener {
+
     private VentanaInicio vistaSesion;
-   
-    public ControlVentanaInicio(VentanaInicio vistaSesion){
+
+    public ControlVentanaInicio(VentanaInicio vistaSesion) {
         this.vistaSesion = vistaSesion;
         this.vistaSesion.getBotonLogin().addActionListener(this);
         this.vistaSesion.getBotonRegistro().addActionListener(this);
+        this.vistaSesion.getBotonCrear().addActionListener(this);
     }
 
     @Override
@@ -27,6 +25,35 @@ public class ControlVentanaInicio implements ActionListener {
             vistaSesion.mostrarLogin();
         } else if (e.getSource() == vistaSesion.getBotonRegistro()) {
             vistaSesion.mostrarRegistro();
+        } else if (e.getSource() == vistaSesion.getBotonCrear()) {
+            if (vistaSesion.getTextoRNombre().getText().trim().isEmpty()
+                    || vistaSesion.getTextoRIdentificacion().getText().trim().isEmpty()
+                    || vistaSesion.getTextoRNumeroTel().getText().trim().isEmpty()
+                    || vistaSesion.getTextoRRolaPET().getText().trim().isEmpty()
+                    || vistaSesion.getTextoRUsuario().getText().trim().isEmpty()
+                    || new String(vistaSesion.getTextoRContraseña().getPassword()).trim().isEmpty()
+                    || new String(vistaSesion.getTextoRConfirmar().getPassword()).trim().isEmpty()
+                    || vistaSesion.getTextoRMarca().getText().trim().isEmpty()
+                    || vistaSesion.getTextoRModelo().getText().trim().isEmpty()) {
+
+                // Mostrar error en el JLabel de la vista
+                vistaSesion.getLblErrorCampo().setText("Todos los campos deben estar llenos.");
+                return;
+            } else {
+                vistaSesion.getLblErrorCampo().setText("");
+            }
+            validarRegistro();
+        }
+    }
+
+    private void validarRegistro() {
+        String pass = new String(vistaSesion.getTextoRContraseña().getPassword());
+        String confirm = new String(vistaSesion.getTextoRConfirmar().getPassword());
+
+        if (!pass.equals(confirm)) {
+            vistaSesion.getLblError().setText("Las contraseñas no coinciden.");
+        } else {
+            vistaSesion.getLblError().setText("");
         }
     }
 }
